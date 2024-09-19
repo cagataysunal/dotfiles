46d40fd00cb21c64f6c08a3bec4ed0f5c21338f8
Çağatay Sunal <cagataysunal9199@gmail.com>
2024-09-19 03:38:52 +0300

    Add turkish keyboard insert helper

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -57,6 +57,7 @@ vim.opt.scrolloff = 10
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', 'ı', 'i') -- For Turkish keyboards
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
